37a045bee6a8c763b283ce35159ba28a42c196ab
viniciusmaggi <36867104+viniciusmaggi@users.noreply.github.com>
2019-10-28 00:45:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ session_start();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema de Login - PHP + MySQL - Canal TI</title>
+    <title>Sistema de Login - PHP + MySQL - VMO</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="css/bulma.min.css" />
     <link rel="stylesheet" type="text/css" href="css/login.css">
@@ -21,7 +21,7 @@ session_start();
             <div class="container has-text-centered">
                 <div class="column is-4 is-offset-4">
                     <h3 class="title has-text-grey">Sistema de Login</h3>
-                    <h3 class="title has-text-grey"><a href="https://youtube.com/canaltioficial" target="_blank">Canal TI</a></h3>
+                    <h3 class="title has-text-grey"><a href="https://youtube.com/canaltioficial" target="_blank">VMO TI</a></h3>
                     <?php
                     if(isset($_SESSION['nao_autenticado'])):
                     ?>
