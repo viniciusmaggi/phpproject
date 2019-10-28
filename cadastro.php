818798074ea59916d9699f6d4cbbc95b6bef956f
viniciusmaggi <36867104+viniciusmaggi@users.noreply.github.com>
2019-10-28 00:46:28 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema de Cadastro - PHP + MySQL - Canal TI</title>
+    <title>Sistema de Cadastro - PHP + MySQL - VMO TI</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="css/bulma.min.css" />
     <link rel="stylesheet" type="text/css" href="css/login.css">
@@ -17,7 +17,7 @@
             <div class="container has-text-centered">
                 <div class="column is-4 is-offset-4">
                     <h3 class="title has-text-grey">Sistema de Cadastro</h3>
-                    <h3 class="title has-text-grey"><a href="https://youtube.com/canaltioficial" target="_blank">Canal TI</a></h3>
+                    <h3 class="title has-text-grey"><a href="https://youtube.com/canaltioficial" target="_blank">VMO TI</a></h3>
                     <div class="notification is-success">
                       <p>Cadastro efetuado!</p>
                       <p>Faça login informando o seu usuário e senha <a href="login.php">aqui</a></p>
